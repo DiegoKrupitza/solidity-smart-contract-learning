28674f9ade341eb9bed5b9d4923c148219b4de47
Diego Krupitza <diego@pinguinnet.at>
2021-02-24 19:29:26 +0100

    fixed some errors

--- migrations/1_initial_migration.js
+++
@@ -1,9 +0,0 @@
-const HelloWorld = artifacts.require("HelloWorld");
-const Dao = artifacts.require("Dao");
-const SimpleCoinFlip = artifacts.require("SimpleCoinFlip");
-
-module.exports = function (deployer) {
-  deployer.deploy(HelloWorld);
-  deployer.deploy(Dao);
-  deployer.deploy(SimpleCoinFlip);
-};
